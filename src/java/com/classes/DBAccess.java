0b1bafca3a2b958007ca1220532acefb762d483b
ashraf <ashraf@ashraf-Inspiron-1564>
2014-03-03 19:26:29 +0200

    commit

---
+++ src/java/com/classes/DBAccess.java
@@ -0,0 +1,75 @@
+package com.classes;
+        
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author antonios
+ */
+public class DBAccess {
+
+    String databaseName = "commerce";
+    String databaseUsername = "root";
+    String databasePassword = "mysql";
+
+    Connection con = null;
+    ResultSet rst = null;
+
+    public Connection StartConnection() throws SQLException {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName, databaseUsername, databasePassword);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return con;
+    }
+
+    public Vector getAllProducts() throws SQLException {
+
+        Vector<Product> products = new Vector<>();
+        con = StartConnection();
+        
+        if (con != null) {
+
+            try {
+                PreparedStatement pst = con.prepareCall("SELECT * FROM product");
+                rst = pst.executeQuery();
+
+                if (rst.first()) {
+                    do {
+                        Product prod = new Product(rst.getInt("id"),
+                                rst.getString("name"),
+                                rst.getString("desc"),
+                                rst.getFloat("price"),
+                                rst.getInt("quantity"),
+                                rst.getInt("c_id"),
+                                rst.getString("picture")
+                        );
+
+                        products.add(prod);
+
+                    } while (rst.next());
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            con.close();
+        }
+
+        return products;
+    }
+
+}
---
+++ src/java/com/classes/Product.java
@@ -0,0 +1,98 @@
+package com.classes;
+
+import javax.websocket.Decoder;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author antonios
+ */
+public class Product {
+
+    private int id;
+    private String name;
+    private String desc;
+    private float price;
+    private int quantity;
+    private int categoryID;
+    private String image;
+
+    public Product(int id, String name, String desc, float price, int quantity, int categoryID, String image) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        this.quantity = quantity;
+        this.categoryID = categoryID;
+        this.image = image;
+    }
+
+    public Product(int id, String name, String desc, float price, int quantity, int categoryID) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        this.quantity = quantity;
+        this.categoryID = categoryID;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+    
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
